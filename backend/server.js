ad59410b19a3833caf42017468de3c38c815fd97
daviizr77-web <daviizr77@gmail.com>
2026-03-18 04:54:04 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
-// LISTAR USERS
-app.get("/users", (req,res)=>{
- res.json(users);
+let historico = [];
+let chat = [];
+
+// APOSTA
+app.post("/apostar", (req,res)=>{
+ let user = users.find(u=>u.email===req.body.email);
+
+ let valor = Number(req.body.valor);
+ user.saldo -= valor;
+
+ let resultado = Math.random() > 0.5;
+
+ if(resultado){
+   user.saldo += valor*2;
+ }
+
+ historico.push({
+   email:user.email,
+   valor,
+   ganhou:resultado
+ });
+
+ res.json(user);
 });
 
-// ADD SALDO ADMIN
-app.post("/add", (req,res)=>{
- let user = users.find(u=>u.email===req.body.email);
- user.saldo += 100;
- res.json(user);
+// HISTÓRICO
+app.get("/historico",(req,res)=>{
+ res.json(historico);
 });
